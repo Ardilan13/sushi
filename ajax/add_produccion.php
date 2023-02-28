bac90370b043d55d458f35a3142ea78aca0570ed
Dilan Corredor <dilancorr@gmail.com>
2023-02-27 20:47:20 -0500

    Update add_produccion.php

--- ajax/add_produccion.php
+++ ajax/add_produccion.php
@@ -9,11 +9,10 @@ $tipo = $_POST["tipo"];
 $unidad = $_POST["unidad"];
 $cantidad = $_POST["cantidad"];
 
-echo $id . ',' . $cantidad_pro1 . ',' . $nombre . ',' . $tipo . ',' . $unidad . ',' . $cantidad;
-
-/* $crear_prod = "INSERT INTO productos(nombre, tipo, unidad, cantidad) VALUES ('$nombre',$tipo,$unidad,$cantidad);";
+$crear_prod = "INSERT INTO productos(nombre, tipo, unidad, cantidad) VALUES ('$nombre',$tipo,$unidad,$cantidad);";
 $resultado = mysqli_query($con, $crear_prod);
-if ($resultado) {
+echo $crear_prod;
+/* if ($resultado) {
     (float)$total1 = (float)$total - (float)$cantidad_pro1;
     $actualizar_prod = "UPDATE productos SET cantidad = $total1 WHERE id = $id";
     $resultado = mysqli_query($con, $actualizar_prod);
