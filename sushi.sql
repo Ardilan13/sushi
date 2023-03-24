0c5da2e31f5c4bf48e9477d17fb8ca6f826036ba
Dilan Corredor <dilancorr@gmail.com>
2023-03-23 23:49:05 -0500

    bd

--- sushi.sql
+++ sushi.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2023 a las 00:27:58
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 7.4.29
+-- Tiempo de generación: 24-03-2023 a las 05:48:43
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,17 @@ CREATE TABLE `compra` (
   `precio` double(50,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`id`, `id_producto`, `fecha`, `cantidad`, `precio`) VALUES
+(2, 1, '2023-03-23', 1000.0, 1200.00),
+(3, 1, '2023-03-23', 1000.0, 2000.00),
+(4, 1, '2023-03-22', 1000.0, 1500.00),
+(5, 1, '2023-03-23', 1100.0, 1100.00),
+(6, 1, '2023-03-23', 100.0, 1000.00);
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +61,15 @@ CREATE TABLE `cuentas` (
   `valor` double(50,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`id`, `tipo`, `id_preparacion`, `id_diario`, `cantidad`, `valor`) VALUES
+(1, 0, 1, 1, 1000.00, 100.00),
+(2, 0, 1, 2, 150.00, 1200.00),
+(3, 0, 1, 2, 10.00, 1000.00);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +83,14 @@ CREATE TABLE `diario` (
   `status` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `diario`
+--
+
+INSERT INTO `diario` (`id`, `fecha`, `valor`, `status`) VALUES
+(1, '2023-03-23', 100.00, 1),
+(2, '2023-03-24', 11200.00, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -74,8 +102,8 @@ CREATE TABLE `ingredientes` (
   `tipo` int(1) NOT NULL,
   `id_preparacion` int(100) NOT NULL,
   `id_producto` int(100) NOT NULL,
-  `cantidad` float(10,2) NOT NULL,
-  `valor` float(10,2) NOT NULL
+  `cantidad` int(100) NOT NULL,
+  `valor` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -83,20 +111,11 @@ CREATE TABLE `ingredientes` (
 --
 
 INSERT INTO `ingredientes` (`id`, `tipo`, `id_preparacion`, `id_producto`, `cantidad`, `valor`) VALUES
-(1, 0, 1, 1, 1.00, 1.00),
-(2, 0, 1, 1, 2.00, 3.00),
-(3, 1, 1, 2, 2.00, 3.00),
-(4, 0, 2, 1, 100.00, 2.00),
-(5, 0, 2, 1, 11.00, 10.50),
-(6, 0, 2, 1, 10.00, 1000.00),
-(7, 0, 2, 1, 11.45, 1000.00),
-(8, 0, 2, 1, 10.00, 100.00),
-(9, 0, 2, 1, 100.00, 100.00),
-(10, 0, 1, 1, 100.00, 100.00),
-(11, 0, 3, 1, 10.00, 100.00),
-(12, 0, 3, 1, 50.00, 20.00),
-(13, 0, 4, 1, 100.00, 10.00),
-(14, 1, 4, 3, 10.00, 100.00);
+(1, 0, 1, 1, 10, 100),
+(2, 0, 1, 1, 5, 1000),
+(3, 0, 3, 1, 5, 100),
+(4, 0, 3, 3, 1, 500),
+(5, 0, 2, 4, 5, 100);
 
 -- --------------------------------------------------------
 
@@ -112,6 +131,14 @@ CREATE TABLE `movimientos` (
   `cantidad` double(10,2) NOT NULL,
   `motivo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `movimientos`
+--
+
+INSERT INTO `movimientos` (`id`, `id_producto`, `tipo`, `fecha`, `cantidad`, `motivo`) VALUES
+(1, 1, 1, '2023-03-23', 100.00, 'asd'),
+(2, 1, 1, '2023-03-23', 3010.00, 'asda');
 
 -- --------------------------------------------------------
 
@@ -133,10 +160,12 @@ CREATE TABLE `preparaciones` (
 --
 
 INSERT INTO `preparaciones` (`id`, `nombre`, `tipo`, `unidad`, `cantidad`, `valor`) VALUES
-(1, 'dilan', 2, 1, NULL, 10013.00),
-(2, 'redes de computadores', 1, 1, NULL, 32512.50),
-(3, 'pureba_costo', 1, 3, NULL, 2000.00),
-(4, 'prueba_receta', 2, 3, NULL, 2000.00);
+(1, 'prueba_sushi', 1, 1, NULL, 6000.00),
+(2, 'prueba', 2, 1, NULL, 500.00),
+(3, 'dilan', 1, 1, NULL, 1000.00),
+(4, 'dilan', 2, 2, NULL, NULL),
+(5, 'prueba12345', 1, 3, NULL, NULL),
+(6, 'SISTEMAS DIGITALES123456', 2, 3, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -148,10 +177,10 @@ CREATE TABLE `productos` (
   `id` int(100) NOT NULL,
   `nombre` varchar(40) NOT NULL,
   `status` int(2) DEFAULT NULL,
-  `tipo` int(2) NOT NULL,
+  `tipo` int(2) DEFAULT NULL,
   `proveedor` text DEFAULT NULL,
   `unidad` int(2) NOT NULL,
-  `merma` int(4) NOT NULL DEFAULT 0,
+  `merma` int(4) DEFAULT 0,
   `cantidad` double(11,2) NOT NULL,
   `precio` double(50,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -161,7 +190,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `status`, `tipo`, `proveedor`, `unidad`, `merma`, `cantidad`, `precio`) VALUES
-(1, 'redes de computadores', NULL, 1, 'a', 1, 0, 1.00, 1.00);
+(1, 'prueba', 0, 1, 'si', 1, 0, 50.00, 1000.00),
+(3, 'prueba_sushi', NULL, 0, NULL, 1, 0, 0.00, 6000.00),
+(4, 'dilan', NULL, 0, NULL, 1, 0, 15.00, 1000.00);
 
 -- --------------------------------------------------------
 
@@ -179,7 +210,7 @@ CREATE TABLE `tipo` (
 --
 
 INSERT INTO `tipo` (`id`, `nombre`) VALUES
-(1, 'hola');
+(1, 'prueba');
 
 --
 -- Índices para tablas volcadas
@@ -245,43 +276,43 @@ ALTER TABLE `tipo`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `diario`
 --
 ALTER TABLE `diario`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `ingredientes`
 --
 ALTER TABLE `ingredientes`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `preparaciones`
 --
 ALTER TABLE `preparaciones`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo`
