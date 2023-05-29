c597b72aff70ec865cf5ff03b6e4a17ab86e0ed6
Ardilan <dilancorr@gmail.com>
2023-05-29 17:18:47 -0500

    Update add_compra.php

--- ajax/add_compra.php
+++ ajax/add_compra.php
@@ -8,21 +8,22 @@ $cantidad = $_POST["cantidad"];
 $precio = $_POST["valor"];
 $status = null;
 
-$crear_compra = "INSERT INTO compra(id_producto, fecha, cantidad, precio) VALUES ($producto,'$fecha',$cantidad,$precio);";
+$get_producto = "SELECT cantidad, precio, merma FROM productos WHERE id = $producto LIMIT 1;";
+$resultado_get = mysqli_query($con, $get_producto);
+$row = mysqli_fetch_array($resultado_get);
+$cantidad_merma = $cantidad - ((float)$cantidad * (float)$row['merma']) / 100;
+$total = (float)$row['cantidad'] + ((float)$cantidad - (((float)$cantidad * (float)$row['merma']) / 100));
+$precioU = (((float)$row['precio'] * (float)$row['cantidad']) + ((float)$precio * (((float)$cantidad * (float)$row['merma']) / 100))) / $total;
+if ($row['precio'] > $precioU) {
+    $status = 0;
+} else if ($row['precio'] < $precioU) {
+    $status = 1;
+} else {
+    $status = 2;
+}
+$crear_compra = "INSERT INTO compra(id_producto, fecha, cantidad, precio) VALUES ($producto,'$fecha',$cantidad_merma,$precio);";
 $resultado = mysqli_query($con, $crear_compra);
 if ($resultado) {
-    $get_producto = "SELECT cantidad, precio, merma FROM productos WHERE id = $producto LIMIT 1;";
-    $resultado_get = mysqli_query($con, $get_producto);
-    $row = mysqli_fetch_array($resultado_get);
-    $total = (float)$row['cantidad'] + ((float)$cantidad - (((float)$cantidad * (float)$row['merma']) / 100));
-    $precioU = (((float)$row['precio'] * (float)$row['cantidad']) + ((float)$precio * (((float)$cantidad * (float)$row['merma']) / 100))) / $total;
-    if ($row['precio'] > $precioU) {
-        $status = 0;
-    } else if ($row['precio'] < $precioU) {
-        $status = 1;
-    } else {
-        $status = 2;
-    }
     $update_producto = "UPDATE productos SET status = $status, cantidad=$total, precio=$precioU WHERE id = $producto;";
     $resultado_update = mysqli_query($con, $update_producto);
     if ($resultado_update) {
